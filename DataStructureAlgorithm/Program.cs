43893afd02b9e9611f59e3409939ad7b76f50aed
AartiPundalik <aartipundalik68@gmail.com>
2023-03-02 15:19:55 +0530

    [Aarti] Add : UC1 - Bubble Sort

---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Welcome\n" +
+                     "Choose 3L For BUbble sorting");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BubbleSort.Sort();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
